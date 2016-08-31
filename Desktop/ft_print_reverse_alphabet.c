f00991a3375dd3ee30adddfcf9b60a08698ca740
Medi MEGUENNI <mmeguenn@e2r4p14.42.fr>
2016-08-31 10:01:34 +0200

    Exo 0 J02 Meguenni

---
+++ Desktop/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+//
+//  ft_print_reverse_alphabet.c
+//  
+//
+//  ExO 01
+//
+//
+
+int ft_putchar( char c);
+
+void ft_print_reverse_alphabet(void)
+
+char z;
+
+z = 'z';
+
+while (z >= 'a')
+
+{
+    
+    ft_putchar(z);
+    z--;
+}
